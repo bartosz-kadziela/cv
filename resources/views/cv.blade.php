7b0c98d68bdd5e382b84ebe6ea2eb2abf618d968
Bartosz Kądziela <bartosz.kadziela@o2.pl>
2024-03-24 22:13:22 +0100

    Add CV page and remove Welcome page

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <title>
+            Bartosz Kądziela - CV
+        </title>
+    </head>
+    <body class="bg-slate-100">
+    </body>
+</html>
