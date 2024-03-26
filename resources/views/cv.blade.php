7529ad3f8367304f5d56cd10d158ace20ca43856
Bartosz Kądziela <bartosz.kadziela@o2.pl>
2024-03-26 18:23:50 +0100

    Resize height of header div and its' elements. Add headers, text and styling to main div

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -12,14 +12,14 @@
     </head>
     <body class="bg-[url('/images/background.svg')]">
         <div class="flex flex-col w-full max-w-2xl mx-auto">
-            <header class="relative bg-red-700" style="height: 180px">
-                <img src="/images/Cv_photo.jpg" alt="CV avatar photo" class="absolute inset-0 rounded-full border-8 border-white m-2" style="height: 220px">
-                <div class="pl-64 pr-4 pt-10 text-white flex flex-col">
-                    <p class="tracking-wide text-4xl tracking-widest font-semibold" style="font-family:Poppins">
+            <header class="relative bg-red-700" style="height: 140px">
+                <img src="/images/Cv_photo.jpg" alt="CV avatar photo" tabindex="0" class="focus:animate-bounce absolute inset-0 rounded-full border-8 border-white hover:border-black m-2" style="height: 180px">
+                <div class="pl-60 pr-4 pt-4 text-white flex flex-col">
+                    <p class="tracking-wide tracking-widest font-semibold" style="font-family:Poppins; font-size:37px">
                         BARTOSZ KĄDZIELA
                     </p>
-                    <div class="mt-4 flex flex-col">
-                        <div class="flex flex-row gap-2" style="font-size:9.3px">
+                    <div class="mt-2 flex flex-col">
+                        <div class="flex flex-row gap-2" style="font-size:10px">
                             <div class="flex flex-row gap-1 hover:animate-pulse">
                                 <img src="/images/phone.svg" alt="Phone icon" class="h-4">
                                 <a href="tel:+48506106848" class="hover:underline drop-shadow-xl">
@@ -50,7 +50,7 @@
                     </div>
                 </div>
             </header>
-            <main class="bg-white flex flex-row" style="height: 560px">
+            <main class="bg-white flex flex-row" style="height: 600px">
                 <div class="flex flex-col mt-12 mx-8 w-1/4">
                     <h1 class="text-lg font-bold" style="font-family:Poppins">
                         About Me
@@ -64,15 +64,15 @@
                         projects to further enhance myself in
                         these areas.
                     <p>
-                    <div class="pt-2 font-normal" style="font-size:10px">
-                        <div class="flex inline-flex">
-                            <img src="/images/locationpin.svg" alt="Locationpin icon" class="h-4 mr-0.5 hover:animate-spin">
+                    <div class="pt-2 font-normal border-b" style="font-size:10px">
+                        <div class="flex inline-flex focus:animate-bounce" tabindex="0">
+                            <img src="/images/locationpin.svg" alt="Locationpin icon" class="h-4 mr-0.5">
                             <p>
                                 Lodz,Poland
                             </p>
                         </div>
-                        <div class="flex inline-flex">
-                            <img src="/images/calendar.svg" alt="Calendar icon" class="h-4 ml-2 mr-1 hover:animate-spin">
+                        <div class="flex inline-flex focus:animate-bounce" tabindex="0">
+                            <img src="/images/calendar.svg" alt="Calendar icon" class="h-4 ml-2 mr-1 focus:animate-bounce">
                             <p>
                                 26/05/1992
                             </p>
@@ -85,18 +85,54 @@
                     </h1>
                     <div class="flex flex-row">
                         <div class="flex flex-col pr-4 pt-2.5 items-center">
-                            <div class="border-2 border-black h-3 w-3 rounded-full"style="border-width:1,5px">
+                            <button class="hover:bg-black focus:animate-pulse focus:ring focus:ring-slate-500 border-2 border-black h-3 w-3 rounded-full"style="border-width:1,5px">
+                            </button>
+                            <div class="bg-black" style="width:1px; height: 152px">
                             </div>
-                            <div class="bg-black h-96" style="width:1px">
-                            </div>
+                            <button class="hover:bg-black focus:animate-pulse focus:ring focus:ring-slate-500 border-2 border-black h-3 w-3 rounded-full"style="border-width:1,5px">
+                            </button>
                         </div>
                         <div class="flex flex-col">
                             <p class= "pt-1" style="font-family:Poppins">
                                 11/2021 - 03/2024 | Takeda S.C.E
                             </p>
-                            <h1 h1 class="text-md font-bold" style="font-family:Poppins">
+                            <h1 class="font-bold pb-1" style="font-family:Poppins">
                                 Business Analyst (Service Now)
                             </h1>
+                            <ul class="list-none mr-6 font-light" style="font-size:10px">
+                                <li>- Collecting User Requirements - meetings with stakeholders</li>
+                                <li>- Managing User Stories in Service Now Visual Task Board (Agile)</li>
+                                <li>- Preparing Solution design mock-ups for ServiceNow enhancements</li>
+                                <li>- Creating Acceptance Criteria for the development team</li>
+                                <li>- Testing ServiceNow enhancements in Development/QA/Test environments</li>
+                                <li>- Creating ServiceNow Reports, Dashboards and Knowledge Articles</li>
+                                <li class="mb-1">- Holding Peer Review sessions for more complex enhancements</li>
+                            </ul>
+                            <p class= "pt-1" style="font-family:Poppins">
+                                08/2019 - 10/2021 | Fujitsu Technology Solutions
+                            </p>
+                            <h1 class="font-bold pb-1" style="font-family:Poppins">
+                                Technical Operations Specialist (Master Data)
+                            </h1>
+                            <ul class="list-none mr-6 font-light" style="font-size:10px">
+                                <li>- Implementing and configuring Primergy/Primequest servers in SAP PP/MM</li>
+                                <li>- Maintaining data throughout the whole Product Life Cycle</li>
+                                <li>- Coordinating data transfer between SharePoint, JPDM, SAP environments</li>
+                                <li class="mb-1">- Cooperating with Product Management Team from Germany and Japan </li>
+                            </ul>
+                            <p class= "pt-1" style="font-family:Poppins">
+                                08/2019 - 10/2021 | Fujitsu Technology Solutions
+                            </p>
+                            <h1 class="font-bold pb-1" style="font-family:Poppins">
+                                Service Process Controller
+                            </h1>
+                            <ul class="list-none mr-6 font-light" style="font-size:10px">
+                                <li>- Managing and maintaining Service Desk processes</li>
+                                <li>- Reviewing Call Quality and managing incidents</li>
+                                <li>- Managing escalations for UK customer</li>
+                                <li>- Reporting SLA & KPI</li>
+                                <li>- Coordinating Lean Management</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
