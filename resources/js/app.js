b3af4230849ac762de8104095b331a5b295ad37c
Bartosz Kądziela <bartosz.kadziela@o2.pl>
2024-03-29 04:41:29 +0100

    Add page scrolling functionality and styling

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import './bootstrap';
 
 let currentPath = window.location.pathname;
 
+//Collapsible
+
 if (currentPath === "/") {
     var collapsibles = document.getElementsByClassName("collapsible");
 
@@ -19,3 +21,23 @@ if (currentPath === "/") {
         });
     }
 }
+
+//Scroll page
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var scrollToBottomBtn = document.getElementById("scrollToBottomBtn");
+
+    scrollToBottomBtn.addEventListener("click", function() {
+
+        var body = document.body;
+        var html = document.documentElement;
+        var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+        var scrollHeight = 0.45 * height;
+
+        window.scrollTo({
+            top: scrollHeight,
+            behavior: "smooth"
+        });
+    });
+});
